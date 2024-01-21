80b618cefc54695c6ce1985b2e1e8e29d57184ac
RoboAM13 <atul20040m@gmail.com>
2024-01-21 15:48:50 +0530

    firebase progress

--- lib/screens/choose.dart
+++ lib/screens/choose.dart
@@ -1,5 +1,24 @@
-
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void getMachineNames() async {
+  try {
+    // Get a reference to the 'machines' collection
+    CollectionReference machinesCollection =
+        FirebaseFirestore.instance.collection('Vending_Machines');
+
+    // Get the documents in the 'machines' collection
+    QuerySnapshot machinesSnapshot =
+        await machinesCollection.doc("Location_1").collection("items").get();
+
+    // Iterate through the documents and print their names
+    machinesSnapshot.docs.forEach((DocumentSnapshot document) {
+      print('Machine Name: ${document.id}');
+    });
+  } catch (e) {
+    print('Error getting machine names: $e');
+  }
+}
 
 class Choose extends StatefulWidget {
   const Choose({super.key});
@@ -10,24 +29,19 @@ class Choose extends StatefulWidget {
 
 class _ChooseState extends State<Choose> {
   @override
+  void initState() {
+    super.initState();
+    print("Hello");
+    getMachineNames();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Vending Machine Options')),
-      body: Column(
-        children: [
-        ElevatedButton(child: Text('Go') ,
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/status');
-          }
-
-        )
-        ],
-        
-
-
-      ),
-      
-    );
-    
+        appBar: AppBar(title: Text('Vending Machine')),
+        body: Column(
+          children: [
+            Text("Select the Vending Machine"),
+          ],
+        ));
   }
 }
